434716a52f1ea31af720f8e356fb169f0185cf28
Pranav Vispute <pranavvispute671@gmail.com>
2024-08-09 17:04:31 +0530

    corrected everything

--- challenge-3/main.tf
+++ challenge-3/main.tf
@@ -2,3 +2,20 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+module "dbserver" {                            //creates dbserver from template
+  source = "./dbserver-module"             
+  ec2-name = "DB-Server"
+}
+
+output "module-db-name" {                      //dbserver private id var
+  value = module.dbserver.db-pvt-id
+}
+
+module "wbserver" {
+  source = "./wbserver-module"
+  ec2-name = "WB-Server"
+  script-file = "server-script.sh"
+}
+output "module-wb-server" {
+  value = module.wbserver.wbserver-publicip
+}
