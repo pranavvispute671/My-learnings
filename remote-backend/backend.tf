4dc478d221e35608e8b62fdec2f321d695759c85
Pranav Vispute <pranavvispute671@gmail.com>
2024-08-14 20:00:44 +0530

    commented the scret key lines

--- remote-backend/backend.tf
+++ remote-backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket="gitlab-bucket-3-env"   //name of the bucket
-    key="terraform/tfstate.tfstate"  //folder/name of state file
+    bucket_key="terraform/tfstate.tfstate"  //folder/name of state file(rename it to key)
     region="us-east-1"
   }
 }
