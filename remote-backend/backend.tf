a17ea323e30ab9028cad38ec30be44f71ca7ab09
Pranav Vispute <pranavvispute671@gmail.com>
2024-08-14 19:58:21 +0530

    removed secret key

--- remote-backend/backend.tf
+++ remote-backend/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket="gitlab-bucket-3-env"   //name of the bucket
     key="terraform/tfstate.tfstate"  //folder/name of state file
     region="us-east-1"
-    access_key="AKIA6GBMGAX6CWVGOLH7"
-    secret_key="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"
+    access_key=""
+    secret_key=""
   }
 }
