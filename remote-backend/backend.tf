9a9986612128a5c76bb5d2f595a4ff27207959b1
Pranav Vispute <pranavvispute671@gmail.com>
2024-08-22 11:38:59 +0530

    added backend in remote backend

--- remote-backend/backend.tf
+++ remote-backend/backend.tf
@@ -1,7 +1,10 @@
 terraform {
   backend "s3" {
     bucket="gitlab-bucket-3-env"   //name of the bucket
-    #bucket_key="terraform/tfstate.tfstate"  //folder/name of state file(rename it to key)
+    key="terraform/tfstate.tfstate"  //folder/name of state file(rename it to key)
     region="us-east-1"
+    access_key=""
+    secret_key=""
+
   }
 }
